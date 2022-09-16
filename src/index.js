cd99f34494d1008535de8b68f9d430beb2c9b73c
Harun Doğdu <harundogdu06@gmail.com>
2022-09-16 17:49:03 +0300

    feat(practium) : theme context created and used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'style/style.scss';
-import { Home } from 'containers';
+import { ThemeContext } from 'contexts';
+import { Home } from 'pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />);
+root.render(
+  <ThemeContext>
+    <Home />
+  </ThemeContext>
+);
