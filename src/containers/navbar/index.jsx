cf434c2283f77c0c5fefa7e625c9b2558e7b288c
Harun Doğdu <harundogdu06@gmail.com>
2022-09-16 17:48:31 +0300

    feat(practium) : navbar created and designed

---
+++ src/containers/navbar/index.jsx
@@ -0,0 +1,25 @@
+import { ToggleSwitchButton } from 'components';
+import { useTheme } from 'contexts';
+
+function Navbar() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar__container'>
+        <h3 className='navbar__container-logo'>Credit App</h3>
+        <ul className='navbar__container-menu'>
+          <li>
+            <ToggleSwitchButton
+              name='theme'
+              onChange={toggleTheme}
+              value={theme}
+            />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
