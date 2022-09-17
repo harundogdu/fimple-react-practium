27b7f60de45d3a6ee4866028bc52d43eff84d0db
Harun Doğdu <harundogdu06@gmail.com>
2022-09-17 16:53:28 +0300

    feat(practium) : table-helper created

---
+++ src/utils/table-helper.js
@@ -0,0 +1,9 @@
+export const loanTableHeaderColumns = [
+  { title: 'Taksit No', accessor: 'installmentNumber' },
+  { title: 'Taksit Tutarı', accessor: 'installmentAmount' },
+  { title: 'Anapara', accessor: 'principalAmount' },
+  { title: 'Kalan Ana Para', accessor: 'remainingPrincipalAmount' },
+  { title: 'Kâr Tutarı', accessor: 'profitAmount' },
+  { title: 'KKDF', accessor: 'kkdfAmount' },
+  { title: 'BSMV', accessor: 'bsmvAmount' }
+];
