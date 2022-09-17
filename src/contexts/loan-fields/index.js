bcf5c8b4b0446f155a331b014099546b8b941ff8
Harun Doğdu <harundogdu06@gmail.com>
2022-09-17 15:33:52 +0300

    feat(practium) : montly payment created

--- src/contexts/loan-fields/index.js
+++ src/contexts/loan-fields/index.js
@@ -1,12 +1,13 @@
 import { createContext, useState, useContext } from 'react';
+import { moneyFormat } from 'utils';
 
 const initialState = {
-  loanAmount: 0,
-  loanTerm: 0,
-  loanType: 'weekly',
-  interestRate: 0,
-  taxType: 'bsmv',
-  taxRate: 0
+  loanAmount: 100000,
+  loanTerm: 12,
+  loanType: 'monthly',
+  interestRate: 2.28,
+  taxBSMVRate: 0.1,
+  taxKKDFRate: 0.15
 };
 
 const LoanFieldsContext = createContext(initialState);
@@ -18,7 +19,71 @@ const LoanFieldsProvider = ({ children }) => {
     setLoanState({ ...loanState, [field]: value });
   };
 
-  const value = { loanState, updateLoanState };
+  const loanTypeConvertToDay = type => {
+    switch (type) {
+      case 'weekly':
+        return 7;
+      case 'yearly':
+        return 365;
+      case 'monthly':
+      default:
+        return 30;
+    }
+  };
+
+  const calculateLoan = () => {
+    const { loanAmount, loanTerm, loanType, interestRate } = loanState;
+
+    let loan = [];
+    let remainingLoanTerm = loanTerm;
+    let remainingLoanAmount = loanAmount;
+    const loanTypeDay = loanTypeConvertToDay(loanType);
+
+    while (remainingLoanTerm > 0) {
+      const compoundPayment = parseFloat(
+        remainingLoanAmount *
+          Math.pow(1 + interestRate / 100, loanTypeDay / 30) -
+          remainingLoanAmount
+      );
+      const KKDFPayment = parseFloat(compoundPayment * loanState.taxKKDFRate);
+      const BSMVPayment = parseFloat(compoundPayment * loanState.taxBSMVRate);
+      const rateLast = parseFloat(
+        Number(
+          interestRate / 100 +
+            KKDFPayment / remainingLoanAmount +
+            BSMVPayment / remainingLoanAmount
+        ).toFixed(4)
+      );
+      const montlyPayment = parseFloat(
+        loanAmount *
+          ((rateLast * (1 + rateLast) ** loanTerm) /
+            ((1 + rateLast) ** loanTerm - 1))
+      );
+      const principalPayment = parseFloat(
+        montlyPayment - (compoundPayment + KKDFPayment + BSMVPayment)
+      );
+
+      remainingLoanAmount = parseFloat(remainingLoanAmount - principalPayment);
+
+      loan.push({
+        loanTerm: moneyFormat(loanTerm),
+        montlyPayment: moneyFormat(montlyPayment),
+        principalPayment: moneyFormat(principalPayment),
+        remainingLoanAmount: moneyFormat(remainingLoanAmount),
+        compoundPayment: moneyFormat(compoundPayment),
+        KKDFPayment: moneyFormat(KKDFPayment),
+        BSMVPayment: moneyFormat(BSMVPayment)
+      });
+
+      remainingLoanTerm--;
+    }
+
+    return {
+      loan
+    };
+  };
+
+  const value = { loanState, updateLoanState, calculateLoan };
 
   return (
     <LoanFieldsContext.Provider value={value}>
